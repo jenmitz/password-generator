004f6e5009dbdb6f6603f403df0d8a7f934db9e8
Jennifer Mitzman <jenmitz@gmail.com>
2020-02-25 17:46:11 -0800

    changed password text to black, values now populate correct spot, still no final randomization yet, need to get rid of commas in array

--- function.js
+++ function.js
@@ -1,46 +1,26 @@
 /*
 Problem: Generate a password that uses the user input to decide what type of characters - and how many - to produce.
 IOCE
-	Inputs: number from user (8-58)
+	Inputs: number from user (8-58), checked boxes representing character types
 	Outputs: randomized password using the parameters the user selects
 	Constraints: n/a
-	Edge cases: none that I can see - not complex enough to have them? very straight-forward
+	Edge cases: entering a letter, not entering anything [solution found]
 
 /////////////////////////////////////////////////
 
-> What's happening currently:
-1. Function that listens for a click - when clicked, it will run the main function that generates the PW
-3. If no boxes are checked, console logs an error
-4. If no number(s) or an incorrect number is entered, console logs an error
-5. If box is checked, a string is passed into an array (to later expand upon to determine what values to draw from)
-6. If box is checked, console logs "____ checked" and generates/console logs a single random value from that array
-7. If a box isn't checked, console logs "___ no" and no random value is generated for that box
-
-
 > Needed functionality:
-- Combine the checked boxes values with the entered # of characters to generate final PW [add to empty array?]
-- Have the PW populate the HTML element (output #password) once generated
-
-
-> Possible ways to generate correct amount/types of values:
-[option 1]  Generate from every character type then have the function ignore whatever values are not selected
-[option 2] Combine the checked boxes values with the entered # of characters to generate final PW / add to empty array?
-
-
-> What I can't quite figure out/brainstorming solutions:
-- How to generate the PW with the correct number of values while drawing the values from whatever boxes are selected
-- Could I combine the values of the checked boxes by adding them into one array, then randomizing from the new array?
-
+- Randomize/generate final password by randomizing the "values" array
+- Populate final PW to the HTML element (#password) once generated
 
 */
 
-// event listener to generate password
+// event listener to run function
 document.getElementById("generateBtn").addEventListener("click", generatePassword);
 
-// generate
+// generate password
 function generatePassword() {
 
-    // variables
+    // variables for checked boxes
     var characterLength = document.getElementById("length").value;
     var numericBoxChecked = document.getElementById("numeric").checked;
     var lowercaseBoxChecked = document.getElementById("lowercase").checked;
@@ -52,11 +32,15 @@ function generatePassword() {
         console.log("nice, you entered a good number")
     } else {
         console.log("no! bad number!");
+        // alert("Please enter a number from 8 - 58.");
+        // stop entire function here
     };
 
-    // see if at least one box is checked / error if no character types are checked
+    // check if at least one box is selected / error if no character types are selected
     if (!numericBoxChecked && !lowercaseBoxChecked && !uppercaseBoxChecked && !specialBoxChecked) {
-        console.log("check a goddamn box you heathen")
+        console.log("check a goddamn box you heathen");
+        // alert("Please select at least one character type.");
+        // stop entire function here
     } else {
         console.log("at least 1 box is checked");
     };
@@ -64,18 +48,17 @@ function generatePassword() {
     // input value
     var input = document.getElementById("length").value;
 
-    // random values from each array using ASCII codes (how many depends on input)
+    // random values from each array using ASCII codes (how many values generated depends on user input)
     var letterUppercase = Array.from({length: input}, () => String.fromCharCode(Math.floor(Math.random() * (90 - 65) + 65)));
     var letterLowercase = Array.from({length: input}, () => String.fromCharCode(Math.floor(Math.random() * (122 - 97) + 97)));
     var special = Array.from({length: input}, () => String.fromCharCode(Math.floor(Math.random() * (43 - 33) + 33)));
     var numeric = Array.from({length: input}, () => String.fromCharCode(Math.floor(Math.random() * (57 - 48) + 48)));
 
-    //empty array to be dynamically added to
+    //empty array to be filled with randomized values of checked boxes
     var values = [];
     
     ///////// HOW DO I MAKE THIS MORE DRY? /////////
-    
-    // if a box is checked, add it to array 'values'
+    // if a box is checked, add randomized values to array 'values'
     if (numericBoxChecked) {
         values.push(numeric);
         console.log(numeric);
@@ -95,12 +78,23 @@ function generatePassword() {
 
     console.log("values array has the following values: " + values);
 
+    document.getElementById("password").append(values);
+
+
+
+
+    var noCommas = values.join("");
+    console.log(noCommas);
+
+
+    for (let i = 0; i < input.length; i++) {
+        (Math.floor(Math.random(values)));
+    };
+    
+        
 };
-generatePassword(); 
 
 //////////////////////////////////// RANDOM NOTES / DUMP ///////////////////////////////////////
-//Math.floor(Math.random() * (57 - 48) + 48));
-
 
 // var range = ("8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58");
 // var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; // 26 characters
